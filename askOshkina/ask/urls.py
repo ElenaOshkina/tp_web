20f24c33bca4c21d1a8d595daccf8894c841c617
Elena <oshkina95@gmail.com>
2017-12-12 22:21:33 +0300

    created templates

--- askOshkina/ask/urls.py
+++ askOshkina/ask/urls.py
@@ -3,5 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^hot/$', views.hotQuestions, name='new-questions'), #список hot вопросов
+    url(r'^login/$', views.formLogin, name='login'), # форма логина
+    url(r'^signup/$', views.formSignUp, name='signup'), #форма регистрации
+    url(r'^ask/$', views.formAsk, name='ask-question'), #форма создания вопроса
+    url(r'^question/(?P<question_id>[0-9]+)/$', views.questionDetail, name='question'),#один вопрос со списком ответов
+    url(r'^tag/(?P<tag_name>[a-zA-Z]+)/$', views.tag, name='tag-question'),#вопрос по тегу
+    url(r'^$', views.index, name='main-page'), # список новых вопросов (главная страница)
 ]
--- askOshkina/ask/views.py
+++ askOshkina/ask/views.py
@@ -1,5 +1,59 @@
 from django.http import HttpResponse
-
+from django.template import RequestContext, loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    #return HttpResponse("Список новых вопросов. Главная страница!")
+    context = {
+        'question': 'question',
+        'answers': 'answers',
+        'form': 'form',
+    }
+    template = loader.get_template('base.html')
+    return HttpResponse(template.render(context))
+
+def hotQuestions(request):
+    return HttpResponse("список hot вопросов")
+
+def questionDetail(request, question_id):
+    context = {
+        'question_num': question_id,
+
+    }
+    template = loader.get_template('question.html')
+    return HttpResponse(template.render(context))
+
+def tag(request, tag_name):
+    context = {
+        'tag': tag_name,
+
+    }
+    template = loader.get_template('questionbytag.html')
+    return HttpResponse(template.render(context))
+
+def formLogin(request):
+    context = {
+        'question': 'question',
+        'answers': 'answers',
+        'form': 'form',
+    }
+    template = loader.get_template('Login.html')
+    return HttpResponse(template.render(context))
+
+def formSignUp(request):
+    context = {
+        'question': 'question',
+        'answers': 'answers',
+        'form': 'form',
+    }
+    template = loader.get_template('signup.html')
+    return HttpResponse(template.render(context))
+
+def formAsk(request):
+    context = {
+        'question': 'question',
+        'answers': 'answers',
+        'form': 'form',
+    }
+    template = loader.get_template('ask.html')
+    return HttpResponse(template.render(context))
+
